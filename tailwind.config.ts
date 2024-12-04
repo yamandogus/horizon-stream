e62d1b93f173fd23c3e8330b1c9029a0be618e12
Doğuş Yaman <152088713+yamandogus@users.noreply.github.com>
2024-12-05 00:02:05 +0300

    login page merged

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,14 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        "custom-purple": '#676279',
+        "custom-purple": '#2b2738',
+        "button-purple": '#6e54b5',
+        "input-purple": '#3b364c',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
